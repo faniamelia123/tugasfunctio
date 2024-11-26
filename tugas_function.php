3c313f8da32ec5c4b8e2eb37c5baaa00287ee8b1
faniamelia123 <aisahsiti85627@gmail.com>
2024-11-26 09:52:00 +0700

    Add files via upload

---
+++ tugas_function.php
@@ -0,0 +1,18 @@
+<?php
+
+function menampilkan(){
+  echo "hello word";                                                
+}
+
+function pergi(){
+  echo "\n saya pamit\n";
+}
+
+menampilkan();
+pergi();
+
+function penjumlahan($angka1,$angka2){
+  echo $angka1+$angka2;
+ 
+}
+penjumlahan(5,6);
